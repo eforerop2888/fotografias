3dffcba3a61bd6a19526338128bded25155c266a
root <root@pineda.pineda.com>
2017-05-03 22:31:42 -0500

    Se crea menu para la pagina y se modifican galerias

--- sites/all/modules/core/templates/menu.tpl.php
+++ sites/all/modules/core/templates/menu.tpl.php
@@ -1,5 +1,15 @@
-<ul>
-	<li></li>
-	<li>Inicio</li>
-	<li>Cerrar Sesión</li>
-</ul>
+<div class="menu_f">
+	<div class="logout_content">
+	  <img src="<?php echo $base_url . '/' . $imagenes ?>/images/logout.png" width="20px">
+	</div>
+	<ul class="menu_ul">
+	  <li><?php echo $user_name; ?></li>
+	  <a href="<?php echo $base_url; ?>">
+	    <li>Inicio</li>
+	  </a>
+	  <a href="<?php echo $base_url; ?>/user/logout">
+		<li>Cerrar Sesión</li>
+	  </a>
+	</ul>
+	</div>
+</div>
