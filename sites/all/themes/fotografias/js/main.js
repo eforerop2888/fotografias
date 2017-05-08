f67173bb424c710bab18723ea2db5f4b8af87597
oscarfabian91 <oscfor@eltiempo.com>
2017-05-08 09:55:32 -0500

    Se implementa funcionalidad de pantalla completa en las galerias

--- sites/all/themes/fotografias/js/main.js
+++ sites/all/themes/fotografias/js/main.js
@@ -1,22 +1,16 @@
 
 jQuery = jQuery.noConflict();
 
-
 jQuery(document).ready(function () {
 
-    jQuery(".fancybox").click(function(){
-        //var elem = document.getElementById("h1");
-        var img = jQuery("#img");
-
-        img.onclick = function() {
-            req = img.requestFullScreen || img.webkitRequestFullScreen || img.mozRequestFullScreen;
-            req.call(img);
-        }    
-    })
+    jQuery(document).on('click','.requestfullscreen',function(e) {
+        e.preventDefault();
+        jQuery(this).prev('img').fullscreen();
+            return false;
+    });
     
     jQuery(".view-galeria-cliente img").addClass("img-responsive");
     jQuery(".view-id-galeria_fotos_todas img").addClass("img-responsive");
-    
    
 });
 
