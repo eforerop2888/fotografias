09860dc36ddac4bc7fb1aff45fe6c9b93b3fc1fc
oscar <oscar@debian.pineda.com>
2017-05-14 10:41:38 -0500

    Nube Tags y mansory(Salvatorre)

--- sites/all/themes/fotografias/js/main.js
+++ sites/all/themes/fotografias/js/main.js
@@ -8,23 +8,10 @@ jQuery(document).ready(function () {
         jQuery(this).prev('img').fullscreen();
             return false;
     });
-    jQuery('.view-id-galeria_fotos_todas .view-content').sortablePhotos({
-      selector: '> .my-item-photo',
-    	sortable: true,
-    	padding: 1
-  	});
-
-  	jQuery('.view-id-galeria_cliente .view-content').sortablePhotos({
-      selector: '> .my-item-photo',
-    	sortable: true,
-    	padding: 1
-  	});
-
-    jQuery('.view-id-favoritos .view-content').sortablePhotos({
-      selector: '> .my-item-photo',
-      sortable: true,
-      padding: 1
-    });
+    jQuery('.view-id-galeria_cliente img').addClass('img-responsive');
+    jQuery('.view-id-galeria_fotos_todas img').addClass('img-responsive');
+    jQuery('.view-id-favoritos img').addClass('img-responsive');
+    jQuery('.view-id-fotos_tags img').addClass('img-responsive');
    
 });
 
