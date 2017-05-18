823d85ea8c1d08ae8211c72ae9b73399d9f3abc0
oscarfabian91 <oscfor@eltiempo.com>
2017-05-18 11:22:32 -0500

    Se implementa modulo Mansory

--- sites/all/themes/fotografias/js/main.js
+++ sites/all/themes/fotografias/js/main.js
@@ -8,10 +8,6 @@ jQuery(document).ready(function () {
         jQuery(this).prev('img').fullscreen();
             return false;
     });
-    jQuery('.view-id-galeria_cliente img').addClass('img-responsive');
-    jQuery('.view-id-galeria_fotos_todas img').addClass('img-responsive');
-    jQuery('.view-id-favoritos img').addClass('img-responsive');
-    jQuery('.view-id-fotos_tags img').addClass('img-responsive');
    
 });
 
