f13307636051ee472f37ae7ad224ebdc1db73101
oscarfabian91 <oscfor@eltiempo.com>
2017-06-01 10:58:41 -0500

    Cambio de estilos generales

--- sites/all/themes/fotografias/js/main.js
+++ sites/all/themes/fotografias/js/main.js
@@ -2,6 +2,8 @@
 jQuery = jQuery.noConflict();
 
 jQuery(document).ready(function () {
+    tag = jQuery(".breadcrumb .last a").html();
+	jQuery("#nameTag").html(tag);
 
     jQuery(document).on('click','.requestfullscreen',function(e) {
         e.preventDefault();
