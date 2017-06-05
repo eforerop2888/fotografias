872d8483dab33b6c34017fda09e232f5a38167e4
eforerop2888 <eforerop2888@universidadean.edu.co>
2017-06-04 21:20:02 -0500

    Se cambia de galeria para visualizacion de imagenes

--- sites/all/themes/fotografias/js/main.js
+++ sites/all/themes/fotografias/js/main.js
@@ -10,8 +10,45 @@ jQuery(document).ready(function () {
         jQuery(this).prev('img').fullscreen();
             return false;
     });
-   
+
+    jQuery('div.fullscreen a').fullsizable({
+	  //reloadOnOpen: true,
+	  loop: true
+	});
+
+    jQuery(document).ajaxComplete(function() {
+	//jQuery('.pager-show-more-next a').click(function() {
+		//alert("fds");
+      	jQuery('div.fullscreen a').fullsizable({
+		  	//reloadOnOpen: true,
+		  	loop: true
+		});
+    });
+
+	jQuery(document).on('click','#fullsized_playgallery',function(e) {
+		e.stopPropagation();
+		interval = setInterval(playGallery, 5000);
+		jQuery.cookie("intervalPlay", interval);
+		jQuery('#fullsized_playgallery').replaceWith('<a id="fullsized_stopgallery" href="#playgallery"><i class="fa fa-stop-circle fa-2x" aria-hidden="true"></i></a>');
+	});
+
+	jQuery(document).on('click','#fullsized_stopgallery',function(e) {
+		e.stopPropagation();
+		clearInterval(jQuery.cookie("intervalPlay"));
+		jQuery('#fullsized_stopgallery').replaceWith('<a id="fullsized_playgallery" href="#playgallery"><i class="fa fa-play-circle fa-2x" aria-hidden="true"></i></a>');
+	});
+    
+	function playGallery(){
+		jQuery('#fullsized_go_next').click();
+	}
+
+	jQuery(document).on('click','#fullsized_downloadgallery',function(e) {
+		e.stopPropagation();
+		var imgUrl = jQuery('#fullsized_image_holder img').attr('src');
+		jQuery('#fullsized_downloadgallery').attr('href', 'download_img?url=' + imgUrl);
+	});
 });
+
 
 
 
